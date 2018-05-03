23650d553e8d30b8128df6a731283e765285c604
pandyroddick <dwstein@outlook.com>
2018-05-03 16:15:57 -0600

    Apply Prettier to App and FilterBar components. Refactor FilterBar component for updated filter categories. Delete unused API helper (to be re-visited later).

--- components/App.jsx
+++ components/App.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import FilterBar from './FilterBar';
-import DataContainer from './DataContainer';
+import React from "react";
+import FilterBar from "./FilterBar";
+import DataContainer from "./DataContainer";
 
 class App extends React.Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      searchTerm: '',
-      filterParam: ''
-    }
+      searchTerm: "",
+      filterParam: "Outstanding"
+    };
 
     this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
     this.handleFilterParamChange = this.handleFilterParamChange.bind(this);
   }
 
   handleSearchTermChange(t) {
-    this.setState({searchTerm: t});
-  };
+    this.setState({ searchTerm: t });
+  }
 
   handleFilterParamChange(f) {
-    this.setState({filterParam: f});
-  };
+    this.setState({ filterParam: f });
+  }
 
   render() {
     return (
@@ -31,18 +31,18 @@ class App extends React.Component {
             <li>Notifications</li>
           </ul>
         </div>
-        <FilterBar 
-        searchTerm={this.state.searchTerm}
-        filterParam={this.state.filterParam}
-        onSearchTermChange={this.handleSearchTermChange}
-        onFilterChange={this.handleFilterParamChange} 
+        <FilterBar
+          searchTerm={this.state.searchTerm}
+          filterParam={this.state.filterParam}
+          onSearchTermChange={this.handleSearchTermChange}
+          onFilterChange={this.handleFilterParamChange}
         />
-        <DataContainer 
-        searchTerm={this.state.searchTerm}
-        filterParam={this.state.filterParam}
+        <DataContainer
+          searchTerm={this.state.searchTerm}
+          filterParam={this.state.filterParam}
         />
       </div>
-    )
+    );
   }
 }
 
--- components/FilterBar.jsx
+++ components/FilterBar.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 class FilterBar extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.handleSearchChange = this.handleSearchChange.bind(this);
@@ -13,65 +13,78 @@ class FilterBar extends React.Component {
 
   handleSearchChange(event) {
     this.props.onSearchTermChange(event.target.value);
-  };
+  }
 
   clearSearch() {
-    this.props.onSearchTermChange('');
+    this.props.onSearchTermChange("");
   }
 
   handleFilterChange(event) {
     if (event.target.value === this.props.filterParam) {
-      this.props.onFilterChange('');
+      this.props.onFilterChange("");
     } else {
       this.props.onFilterChange(event.target.value);
     }
-  };
+  }
 
   handleSubmit(event) {
     event.preventDefault();
-  };
+  }
 
-  render () {
-    const filterTypes = ["Assigned", "Unassigned", "Outstanding", "Closed"];
+  render() {
+    const filterTypes = ["Outstanding", "Open", "In Closing", "Closed"];
     const searchTerm = this.props.searchTerm;
     const filterParam = this.props.filterParam;
 
     const activeStyle = {
-      backgroundColor: 'lightblue'
+      backgroundColor: "lightblue"
     };
 
     const inactiveStyle = {
-      backgroundColor: 'transparent'
-    }
+      backgroundColor: "transparent"
+    };
 
     return (
-    <div className="filter-bar">
-    <div className="filter-button-wrapper">
-      <h2>Filter:</h2>
-        {filterTypes.map(filter => {
-          return (<button
-          key={filter}
-          onClick={this.handleFilterChange}
-          value={filter}
-          className="filter-button"
-          style={filterParam === filter ? activeStyle : inactiveStyle }
-          >{filter}</button>)
-        })}
-    </div>
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" value={searchTerm} onChange={this.handleSearchChange} placeholder="Search..."/>
-        {searchTerm
-        ? <button className="filter-button" onClick={this.clearSearch}>Clear Search</button>
-        : ''}
-      </form>
-    </div>
-    )
-  };
-};
+      <div className="filter-bar">
+        <div className="filter-button-wrapper">
+          <h2>Filter:</h2>
+          {filterTypes.map(filter => {
+            return (
+              <button
+                key={filter}
+                onClick={this.handleFilterChange}
+                value={filter}
+                className="filter-button"
+                style={filterParam === filter ? activeStyle : inactiveStyle}
+              >
+                {filter}
+              </button>
+            );
+          })}
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={this.handleSearchChange}
+            placeholder="Search..."
+          />
+          {searchTerm ? (
+            <button className="filter-button" onClick={this.clearSearch}>
+              Clear Search
+            </button>
+          ) : (
+            ""
+          )}
+        </form>
+      </div>
+    );
+  }
+}
 
 FilterBar.propTypes = {
   searchTerm: PropTypes.string,
   filterParam: PropTypes.string
-}
+};
 
 module.exports = FilterBar;
