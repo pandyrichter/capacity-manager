9661980053e9efbd704e777f7cf146e5152f153f
pandyroddick <dwstein@outlook.com>
2018-05-03 16:12:20 -0600

    Add CSS grid to DataContainer component. Add minor CSS adjustments to DataContainer and FilterBar components.

--- components/DataContainer.jsx
+++ components/DataContainer.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import config from '../config';
-import TeamBlock from './TeamBlock';
-import ProjectDetail from './ProjectDetail';
+import React from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import config from "../config";
+import TeamBlock from "./TeamBlock";
+import ProjectDetail from "./ProjectDetail";
 
 class DataContainer extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       projectsLoading: true,
       projects: [],
-      activeTeam: ''
+      activeTeam: ""
     };
 
     this.checkForBatch = this.checkForBatch.bind(this);
     this.handleTeamChange = this.handleTeamChange.bind(this);
-  };
+  }
 
   componentDidMount() {
     this.checkForBatch();
-  };
+  }
 
   /* 
   LOAD DATA
@@ -33,54 +33,61 @@ class DataContainer extends React.Component {
     Stop
   */
 
-  checkForBatch(offset='') {
+  checkForBatch(offset = "") {
     let self = this;
     const apiUrl = `${config.api}${offset}`;
     const apiAuth = `${config.key}`;
 
-    axios.get(apiUrl, {
-      headers: { Authorization: apiAuth }
-    }).then(res => {
-      self.setState({
-        projects: [...self.state.projects, ...res.data.records]
+    axios
+      .get(apiUrl, {
+        headers: { Authorization: apiAuth }
+      })
+      .then(res => {
+        self.setState({
+          projects: [...self.state.projects, ...res.data.records]
+        });
+        self.checkForOffset(res.data.offset);
       });
-      self.checkForOffset(res.data.offset);
-    });
-  };
+  }
 
   checkForOffset(offset) {
     // offset indicates additional records in Airtable
-    offset ? this.checkForBatch(`?offset=${offset}`) : this.setState({projectsLoading: false});
-  };
+    offset
+      ? this.checkForBatch(`?offset=${offset}`)
+      : this.setState({ projectsLoading: false });
+  }
 
   /* 
   HANDLE DATA FILTERING AND SEARCH
   */
 
   handleTeamChange(o) {
-    this.setState({activeTeam: o});
-  };
+    this.setState({ activeTeam: o });
+  }
 
   filterProjectsByTeam(projects, str) {
-    if (str === '') {
+    if (str === "") {
       return projects;
     } else {
       return projects.filter(p => {
-        return p.fields["Office Submitted"] && p.fields["Office Submitted"].includes(str);
+        return (
+          p.fields["Office Submitted"] &&
+          p.fields["Office Submitted"].includes(str)
+        );
       });
     }
-  };
+  }
 
   filterProjectsByStr(projects, prop, str) {
     return projects.filter(p => {
       // FIXME: some props are arrays, so therefore need to .some > .includes
       try {
         return p.fields[prop].toLowerCase().includes(str.toLowerCase());
-      } catch(e) {
+      } catch (e) {
         console.error(e);
       }
     });
-  };
+  }
 
   filterProjectsByStatus(projects, s) {
     switch (s) {
@@ -88,6 +95,7 @@ class DataContainer extends React.Component {
         return projects.filter(p => p.fields["Status Update?"]);
       case "Outstanding":
         return projects.filter(p => !p.fields["Status Update?"]);
+      // FIXME: Following cases were removed
       case "Assigned":
         return projects.filter(p => p.fields["PM Submitted"]);
       case "Unassigned":
@@ -95,10 +103,10 @@ class DataContainer extends React.Component {
       default:
         return projects;
     }
-  };
+  }
 
   sortProjectsByProp(projects, prop) {
-    return projects.sort((a,b) => {
+    return projects.sort((a, b) => {
       let projA = a.fields[prop].toLowerCase();
       let projB = b.fields[prop].toLowerCase();
 
@@ -110,60 +118,84 @@ class DataContainer extends React.Component {
       }
       return 0;
     });
-  };
+  }
 
   render() {
     const teams = ["Boulder", "Chicago", "Dallas", "Latin America"];
     const projects = this.state.projects;
-    const teamProjects = this.filterProjectsByTeam(projects, this.state.activeTeam);
-    const searchedProjects = this.filterProjectsByStr(teamProjects, "Project Name", this.props.searchTerm);
-    const filteredProjects = this.filterProjectsByStatus(searchedProjects, this.props.filterParam);
-    const sortedProjects = this.sortProjectsByProp(filteredProjects, "Project Name");
+    const teamProjects = this.filterProjectsByTeam(
+      projects,
+      this.state.activeTeam
+    );
+    const searchedProjects = this.filterProjectsByStr(
+      teamProjects,
+      "Project Name",
+      this.props.searchTerm
+    );
+    const filteredProjects = this.filterProjectsByStatus(
+      searchedProjects,
+      this.props.filterParam
+    );
+    const sortedProjects = this.sortProjectsByProp(
+      filteredProjects,
+      "Project Name"
+    );
 
     return (
       <div>
-        <div className="team-notification">{
-          !this.state.activeTeam
-          ? <p>All Teams</p>
-          : <p>{this.state.activeTeam}</p>
-        }</div>
-        {this.state.projectsLoading
-         ? <div className="data-container">
+        {this.state.projectsLoading ? (
+          <div className="data-container">
             <p>Loading</p>
-           </div>
-         : <div className="data-container">
-              <div className="graphs-detail">
-                {teams.map(team => {
-                  const teamProjects = projects.filter(project => project.fields["Office Submitted"] && project.fields["Office Submitted"].includes(team));
-                  return (
-                  <TeamBlock 
-                    key={team} 
-                    team={team} 
+          </div>
+        ) : (
+          <div className="content-grid">
+            {/* Visuals */}
+            <div className="visuals-detail">Visuals</div>
+            <div className="teams-detail">
+              {/* Teams */}
+              {teams.map(team => {
+                const teamProjects = projects.filter(
+                  project =>
+                    project.fields["Office Submitted"] &&
+                    project.fields["Office Submitted"].includes(team)
+                );
+                return (
+                  <TeamBlock
+                    key={team}
+                    team={team}
                     projects={teamProjects}
                     activeteam={team === this.state.activeTeam}
                     onTeamChange={this.handleTeamChange}
+                  />
+                );
+              })}
+            </div>
+            {/* Projects */}
+            <div className="projects-detail">
+              <h5>
+                {this.props.filterParam} Projects: {sortedProjects.length}
+              </h5>
+              <div className="projects-detail__overflow">
+                {sortedProjects.map(project => {
+                  return (
+                    <ProjectDetail
+                      key={project.fields["Project Name"]}
+                      project={project}
                     />
                   );
                 })}
               </div>
-              <div className="projects-detail">
-                <h5>{this.props.filterParam} Projects: {sortedProjects.length}</h5>
-                <div className="projects-detail__overflow">
-                  {sortedProjects
-                    .map(project => {
-                    return <ProjectDetail key={project.fields["Project Name"]} project={project} />
-                  })}
-                </div>
-              </div>
-          </div>}
+            </div>
+          </div>
+        )}
       </div>
-    )
-  };
-};
+    );
+  }
+}
 
 DataContainer.propTypes = {
   searchTerm: PropTypes.string,
   filterParam: PropTypes.string
-}
+};
 
 module.exports = DataContainer;
