b5ee8c31af682e416c262e7a02aa53f8f53074ed
pandyroddick <dwstein@outlook.com>
2018-05-10 14:16:36 -0600

    Update TeamBlock component to handle Team OS&E based on property, and placeholder function to display office. Add minor style updates to TeamBlock.

--- components/TeamBlock.jsx
+++ components/TeamBlock.jsx
@@ -21,20 +21,38 @@ class TeamBlock extends React.Component {
     }
   };
 
-  returnArrOfMgrs(records) {
+  returnArrOfMgrs(records, ose = false) {
     let arr = [];
+    let param = "PM Submitted"
+    if (ose) {
+      param = "OS&E"
+    }
     records.forEach(r => {
       try {
-        arr = [...arr, ...r["PM Submitted"]];
+        arr = [...arr, ...r[param]];
       } catch (e) {}
-    })
+    });
     return arr;
   };
 
+  returnArrOfOffice(records) {
+    let arr = [];
+    records.forEach(r => {
+      try {
+        arr = [...arr, ...r["Office Submitted"]];
+      } catch (e) {}
+    });
+    return arr;
+  }
+
  render() {
     const projects = this.props.projects;
-    const pms = this.returnArrOfMgrs(projects);
+    const pms = this.returnArrOfMgrs(projects, this.props.teamIsOSE);
     const uniquePms = _.uniq(pms);
+    // Placeholder for office return
+    const offices = this.returnArrOfOffice(projects);
+    const office = _.uniq(offices);
+    console.log(office);
 
     const activeStyle = {
       border: '2px solid lightblue',
@@ -51,6 +69,7 @@ class TeamBlock extends React.Component {
         onClick={this.handleTeamChange}
         style={this.props.activeteam ? activeStyle : inactiveStyle }
         >
+        <div>{office[0]}</div>
         <h3>{this.props.team}</h3>
         <ul>
           {uniquePms.map(pm => <li>{pm}</li>)}
